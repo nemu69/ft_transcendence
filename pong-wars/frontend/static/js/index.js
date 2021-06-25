5dcd4e6f1f11e2a777c6f8063ab78c82ffac425e
nemu69 <gametwoofficiel@gmail.com>
2021-06-25 17:35:25 +0200

    patch route SPA and add music

--- pong-wars/frontend/static/js/index.js
+++ pong-wars/frontend/static/js/index.js
@@ -10,38 +10,40 @@ import Friend from "./views/Friend.js";
 
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
+// var x = document.getElementById("music").autoplay;
+
 function nodeScriptReplace(node) {
     if ( nodeScriptIs(node) === true ) {
-            node.parentNode.replaceChild( nodeScriptClone(node) , node );
+        node.parentNode.replaceChild( nodeScriptClone(node) , node );
     }
     else {
-            var i = -1, children = node.childNodes;
-            while ( ++i < children.length ) {
-                  nodeScriptReplace( children[i] );
-            }
+        var i = -1, children = node.childNodes;
+        while ( ++i < children.length ) {
+            nodeScriptReplace( children[i] );
+        }
     }
-
+    
     return node;
 }
 function nodeScriptClone(node){
-        var script  = document.createElement("script");
-        script.text = node.innerHTML;
-
-        var i = -1, attrs = node.attributes, attr;
-        while ( ++i < attrs.length ) {                                    
-            script.setAttribute( (attr = attrs[i]).name, attr.value );
-        }
-        return script;
+    var script  = document.createElement("script");
+    script.text = node.innerHTML;
+    
+    var i = -1, attrs = node.attributes, attr;
+    while ( ++i < attrs.length ) {                                    
+        script.setAttribute( (attr = attrs[i]).name, attr.value );
+    }
+    return script;
 }
 
 function nodeScriptIs(node) {
-        return node.tagName === 'SCRIPT';
+    return node.tagName === 'SCRIPT';
 }
 
 const getParams = match => {
     /*const values = match.result.slice(1);
     const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
-
+    
     return Object.fromEntries(keys.map((key, i) => {
         return [key, values[i]];
     }));*/
@@ -63,7 +65,7 @@ const router = async () => {
         { path: "/posts/:id", view: PostView },
         { path: "/logout", view: Logout }
     ];
-
+    
     // Test each route for potential match
     const potentialMatches = routes.map(route => {
         return {
@@ -71,18 +73,18 @@ const router = async () => {
             result: location.pathname.match(pathToRegex(route.path))
         };
     });
-
+    
     let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
-
+    
     if (!match) {
         match = {
             route: routes[0],
             result: [location.pathname]
         };
     }
-
+    
     const view = new match.route.view(getParams(match));
-
+    
     document.querySelector("#app").innerHTML = await view.getHtml();
     nodeScriptReplace(document.getElementsByTagName("body")[0]);
 };
@@ -91,11 +93,15 @@ window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")) {
+        if (e.target.matches("[data-link]") && e.target.nodeName == "A") {
             e.preventDefault();
             navigateTo(e.target.href);
         }
+        else if (e.target.parentElement.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.parentElement.href);
+        }
     });
-
+    
     router();
 });
