f236bf7784d607cbd5ec8230bffe3b632445db93
nemu69 <gametwoofficiel@gmail.com>
2021-07-01 21:36:01 +0200

    setting component

--- pong-wars/frontend/src/app/app.module.ts
+++ pong-wars/frontend/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
 		{path: 'login', component: LoginComponent},
+		{path: 'setting', component: SettingComponent},
 		{path: '**', component: PageNotFoundComponent}
 	  ]),
   ],
