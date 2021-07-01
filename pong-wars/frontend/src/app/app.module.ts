f7b8857dae3232dfa36716957bc40f4027abb1f1
AcensJJ <uentosacens@gmail.com>
2021-07-01 21:35:13 +0200

    404 not found

--- pong-wars/frontend/src/app/app.module.ts
+++ pong-wars/frontend/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
 		{path: 'login', component: LoginComponent},
+		{path: '', redirectTo: '/login', pathMatch: 'full'},
 		{path: '**', component: PageNotFoundComponent}
 	  ]),
   ],
