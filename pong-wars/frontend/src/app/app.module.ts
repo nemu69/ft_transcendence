217dd36bdb502b5a72dfcef15b0320a80c629012
nemu69 <gametwoofficiel@gmail.com>
2021-07-01 22:08:31 +0200

    404 full black

--- pong-wars/frontend/src/app/app.module.ts
+++ pong-wars/frontend/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppComponent,
     LoginComponent,
     PageNotFoundComponent,
-    SettingComponent
+    SettingComponent,
     NavbarComponent
   ],
   imports: [
