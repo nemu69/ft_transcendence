2981ce7fc235cfc7a61c32f6fe5a1bec9fe80bb7
AcensJJ <uentosacens@gmail.com>
2021-07-01 20:22:49 +0200

    single page app angular

--- pong-wars/frontend/src/app/app.module.ts
+++ pong-wars/frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+		{path: 'login', component: LoginComponent},
+		{path: '**', component: PageNotFoundComponent}
+	  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
