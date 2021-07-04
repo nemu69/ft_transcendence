24891705643edb5b0dd208d4adbec880fd65daeb
nemu69 <gametwoofficiel@gmail.com>
2021-07-04 03:20:18 +0200

    all page-animation logout-redirect login

--- pong-wars/frontend/src/app/app.module.ts
+++ pong-wars/frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,6 +9,26 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { SettingComponent } from './setting/setting.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MatchComponent } from './match/match.component';
+import { StatsComponent } from './stats/stats.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendComponent } from './friend/friend.component';
+
+const routes: Routes = [
+
+	{path: 'login', component: LoginComponent},
+	{path: 'register', component: RegisterComponent},
+	{path: 'setting', component: SettingComponent},
+	{path: 'logout', component: LogoutComponent},
+	{path: 'match', component: MatchComponent},
+	{path: 'profile', component: ProfileComponent},
+	{path: 'stats', component: StatsComponent},
+	{path: 'friend', component: FriendComponent},
+	{path: '', redirectTo: '/login', pathMatch: 'full'},
+	{path: '**', component: PageNotFoundComponent}
+
+  ]
 
 @NgModule({
   declarations: [
@@ -16,19 +37,22 @@ import { RegisterComponent } from './register/register.component';
     PageNotFoundComponent,
     SettingComponent,
     NavbarComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogoutComponent,
+    MatchComponent,
+    StatsComponent,
+    ProfileComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-		{path: 'login', component: LoginComponent},
-		{path: 'register', component: RegisterComponent},
-		{path: 'setting', component: SettingComponent},
-		{path: '', redirectTo: '/login', pathMatch: 'full'},
-		{path: '**', component: PageNotFoundComponent}
-	  ]),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
