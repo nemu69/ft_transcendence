4ccd5755481ce885739984b58784624bf69aa0fe
nemu69 <gametwoofficiel@gmail.com>
2021-09-27 18:30:45 +0200

    button block

--- frontend/src/app/private/components/friend/friend.component.ts
+++ frontend/src/app/private/components/friend/friend.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/public/services/auth-service/auth.service';
+import { UserService } from 'src/app/public/services/user-service/user.service';
+import { FriendsService } from '../../services/friends-service/friends.service';
 
 @Component({
   selector: 'app-friend',
@@ -7,7 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private userService: UserService,
+		private authService: AuthService,
+		private friendsService: FriendsService,
+		) { }
 
   ngOnInit(): void {
   }
