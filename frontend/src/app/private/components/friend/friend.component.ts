a80b73e2a78a7db53aa19b526e6dd260c21024c0
nemu69 <gametwoofficiel@gmail.com>
2021-09-28 15:19:42 +0200

    begin friend interface

--- frontend/src/app/private/components/friend/friend.component.ts
+++ frontend/src/app/private/components/friend/friend.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { UserI } from 'src/app/model/user/user.interface';
 import { AuthService } from 'src/app/public/services/auth-service/auth.service';
 import { UserService } from 'src/app/public/services/user-service/user.service';
 import { FriendsService } from '../../services/friends-service/friends.service';
@@ -21,7 +24,39 @@ export class FriendComponent implements OnInit {
 		private friendsService: FriendsService,
 		) { }
 
-  ngOnInit(): void {
-  }
+  
+
+	user : Observable<UserI>;
+	imageToShow: any;
+	isImageLoading : boolean;
+	ngOnInit(): void {
+		this.authService.getUserId().pipe(
+		  switchMap((idt: number) => this.userService.findOne(idt).pipe(
+			tap((user) => {
+			  this.user = this.userService.findOne(user.id);
+			  this.getImageFromService(user.id);
+			})
+		  ))
+		).subscribe();
+	  }	
+
+	  createImageFromBlob(image: Blob) {
+		let reader = new FileReader();
+		reader.addEventListener("load", () => {
+		   this.imageToShow = reader.result;
+		}, false);
+		if (image) {
+		   reader.readAsDataURL(image);
+		}
+	}
+	getImageFromService(id:number) {
+		this.isImageLoading = true;
+		this.userService.getImage("/api/users/avatarById/" + id.toString()).subscribe(data => {
+			this.createImageFromBlob(data);
+			this.isImageLoading = false;
+		}, error => {
+			this.isImageLoading = false;
+		});
+	}
 
 }
